752eddccdccdefb12077c9ef5e4b7a13df08adb0
amit negi <amitnegi@gmail.com>
2025-07-07 20:36:23 +0530

    3

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ def subs(a, b):
     return a - b
 
 
-result1= add(4, 5)
-result2= subs(5, 2)
+result1 = add(4, 5)
+result2 = subs(5, 2)
 
 
 print(f"result1 - {result1}")
 print(f"result2 - {result2}")
+
